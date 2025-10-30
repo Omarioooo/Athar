1d68fe86953841f7154bff1777b768bf088be2f0
Kalosha <youssef.waeel.haroon@gmail.com>
2025-10-30 22:59:56 +0300

    Revision to Charities

--- AtharPlatform/AtharPlatform/Repositories/CampaignRepository.cs
+++ AtharPlatform/AtharPlatform/Repositories/CampaignRepository.cs
@@ -1,6 +1,4 @@
 ﻿using System.Linq.Expressions;
-using System.Linq;
-using Microsoft.EntityFrameworkCore;
 using AtharPlatform.Models.Enum;
 namespace AtharPlatform.Repositories
 {
@@ -28,18 +26,74 @@ namespace AtharPlatform.Repositories
         }
         public override async Task<List<Campaign>> GetAllAsync()
         {
-              return await _dbSet
-                .Include(c => c.Charity) 
+            return await _dbSet
+              .Include(c => c.Charity)
+              .ToListAsync();
+        }
+
+        public async Task<List<Campaign>> GetByType(CampaignCategoryEnum type)
+        {
+            return await _dbSet
+            .Where(c => c.Category == type)
+            .Include(c => c.Charity)
+            .ToListAsync();
+        }
+
+        public async Task<List<Campaign>> GetPageAsync(string? query, int page, int pageSize)
+        {
+            if (page <= 0) page = 1;
+            if (pageSize <= 0) pageSize = 12;
+
+            var q = _dbSet
+                .Include(c => c.Charity)
+                .AsQueryable();
+
+            if (!string.IsNullOrWhiteSpace(query))
+            {
+                var term = query.Trim();
+                q = q.Where(c =>
+                    c.Title.Contains(term) ||
+                    c.Description.Contains(term) ||
+                    c.Charity.Name.Contains(term));
+            }
+
+            return await q
+                .OrderBy(c => c.Title)
+                .Skip((page - 1) * pageSize)
+                .Take(pageSize)
                 .ToListAsync();
         }
 
-    public async Task<List<Campaign>> GetByType(CampaignCategoryEnum type)
-    {
-        return await _dbSet
-        .Where(c => c.Category == type)
-        .Include(c => c.Charity)
-        .ToListAsync();
-    }
+        public async Task<int> CountAsync(string? query)
+        {
+            var q = _dbSet.AsQueryable();
+
+            if (!string.IsNullOrWhiteSpace(query))
+            {
+                var term = query.Trim();
+                q = q.Where(c =>
+                    c.Title.Contains(term) ||
+                    c.Description.Contains(term) ||
+                    c.Charity.Name.Contains(term));
+            }
+
+            return await q.CountAsync();
+        }
+
+        public  async Task<List<Campaign>> GetByDateAsync(bool latestFirst)
+        {
+            var query = _context.Campaigns
+                    .Include(c => c.Charity)
+                    .AsQueryable();
+
+            if (latestFirst)
+                query = query.OrderByDescending(c => c.StartDate);
+            else
+                query = query.OrderBy(c => c.StartDate);
+
+            return await query.ToListAsync();
+        }
+
 
     }
 }
--- AtharPlatform/AtharPlatform/Repositories/ICampaignRepository.cs
+++ AtharPlatform/AtharPlatform/Repositories/ICampaignRepository.cs
@@ -5,6 +5,11 @@ namespace AtharPlatform.Repositories
 {
     public interface ICampaignRepository : IRepository<Campaign>
     {
+        Task<List<Campaign>> GetPageAsync(string? query, int page, int pageSize);
+        Task<int> CountAsync(string? query);
+        Task<List<Campaign>> GetByDateAsync(bool latestFirst);
+
         Task<List<Campaign>> GetByType(CampaignCategoryEnum type);
+
     }
 }
