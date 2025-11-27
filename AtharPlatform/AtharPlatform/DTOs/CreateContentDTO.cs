769409c9f09eb2429154c691ce1214ce6f8b08db
Kalosha <youssef.waeel.haroon@gmail.com>
2025-11-27 17:26:49 +0200

    Edit Search in Media_3

--- AtharPlatform/AtharPlatform/DTOs/CreateContentDTO.cs
+++ AtharPlatform/AtharPlatform/DTOs/CreateContentDTO.cs
@@ -4,12 +4,14 @@ namespace AtharPlatform.DTOs
 {
     public class CreateContentDTO
     {
+        [Required]
         public string Title { get; set; }
 
+        [Required]
         public string Description { get; set; }
         [Required(ErrorMessage = "CampaignId is required")]
         public int CampaignId { get; set; }
-
+        [Required]
         public IFormFile? PostImage { get; set; }  
     }
 }
